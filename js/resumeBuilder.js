04ece311dec629ad52e584f89e009d2a04607c2f
Maxim Topciu <maximtop@gmail.com>
2016-05-27 11:51:54 +0300

    finishes lections about data types

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,106 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var work = {
+    "jobs": [
+        {
+            "employer": "SRL Inrecons",
+            "title": "Engineer Quantity Surveyor",
+            "location": "Chishinau, Moldova",
+            "dates": "2008-2010",
+            "description": "Preparing estimation documents"
+        },
+        {
+            "employer": "Nika-Stroi 02",
+            "title": "SEO",
+            "location": "Moscow, Russia",
+            "dates": "2010-2013",
+            "description": "Increasing e-commerce sales"
+        },
+        {
+            "employer": "Luna",
+            "title": "Sales and Purchasing Manager",
+            "location": "Moscow, Russia",
+            "dates": "2013-2016",
+            "description": "Searching providers with better prices"
+        }
+    ]
+};
+
+var projects = {
+    "projects": [
+        {
+            "title": "Test1",
+            "dates": "2016",
+            "description": "Description1",
+            "images": [
+                "https://placeimg.com/300/200/tech",
+                "https://placeimg.com/300/200/tech",
+                "https://placeimg.com/300/200/tech"
+            ]
+        },
+        {
+            "title": "Test2",
+            "dates": "2015",
+            "description": "Description2",
+            "images": [
+                "https://placeimg.com/300/200/tech",
+                "https://placeimg.com/300/200/tech",
+                "https://placeimg.com/300/200/tech"
+            ]
+        }
+    ]
+};
+
+var bio = {
+    "name": "Maxim Topciu",
+    "role": "Web Developer",
+    "welcomeMessage": "Hi, everyone! I can do everything",
+    "contacts": {
+        "mobile": "+7-926-934-08-25",
+        "email": "maximtop@gmail.com",
+        "github": "maximtop",
+        "twitter": "maximtop",
+        "location": "Moscow, Russia"
+    },
+    "skills": [
+        "PHP",
+        "Javascript",
+        "HTML",
+        "CSS"
+    ]
+};
+
+var education = {
+    "schools": [
+        {
+            "name": "M.Guboglu Lyceum",
+            "location": "Cheadir-Lunga, Moldova",
+            "degree": "Bacalaureat",
+            "majors": ["General"],
+            "dates": "2003-2006",
+            "url": "http://rtlguboglo.edu-cl.net/"
+
+        },
+        {
+            "name": "Technical University of Moldova",
+            "location": "Chishinau, Moldova",
+            "degree": "Liceciate",
+            "majors": ["Business and Administration"],
+            "dates": "2006-2009",
+            "url": "http://www.utm.md/"
+        }
+    ],
+    "onlineCourses": [
+            {
+                "title": "Javascript Basics",
+                "school": "Udacity",
+                "dates": "2016",
+                "url": "https://classroom.udacity.com/courses/ud804/"
+            },
+            {
+                "title": "PHP",
+                "school": "Hexlet",
+                "dates": "2016",
+                "url": "https://ru.hexlet.io/"
+            }
+        ]
+};
+
